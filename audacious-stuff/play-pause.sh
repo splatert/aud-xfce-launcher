7774e96fc7d9d76ddeba36b04ff6e55960c2b949
splatert <82643571+splatert@users.noreply.github.com>
2024-04-03 08:42:49 -0400

    Add files via upload

--- audacious-stuff/play-pause.sh
+++ audacious-stuff/play-pause.sh
@@ -1,6 +1,6 @@
 audacious --play-pause
 
-artist=$(audtool current-song)
+track=$(audtool current-song)
 album=$(audtool --current-song-tuple-data album)
 
 st=$(audtool playback-status)
@@ -8,5 +8,5 @@ st=$(audtool playback-status)
 
 if [ "$st" = "playing" ]; then
     cover=$(find /tmp -name "audacious*" 2>/dev/null | tail -n 1)
-    notify-send -i "$cover" "$artist" "$album" -t 3000
+    notify-send -i "$cover" "$album"  "$track" -t 3000
 fi
