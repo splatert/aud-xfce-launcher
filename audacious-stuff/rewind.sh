d262e0a1d10ba62707ed6685d6c247dbd5ba420b
splatert <82643571+splatert@users.noreply.github.com>
2024-04-03 08:44:29 -0400

    Add files via upload

--- audacious-stuff/rewind.sh
+++ audacious-stuff/rewind.sh
@@ -3,8 +3,8 @@ audacious --play
 
 sleep .25
 
-artist=$(audtool current-song)
+track=$(audtool current-song)
 album=$(audtool --current-song-tuple-data album)
 
 cover=$(find /tmp -name "audacious*" 2>/dev/null | tail -n 1)
-notify-send -i "$cover" "$artist" "$album" -t 3000
+notify-send -i "$cover" "$album"  "$track" -t 3000
